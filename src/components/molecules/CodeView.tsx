af49088216ccdc88f1e05265d0d249727faf6179
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-03-07 18:57:19 +0900

    refactor: :recycle: atoms, molecules, organism, template directory is included in component directory.

--- src/components/molecules/CodeView.tsx
+++ src/components/molecules/CodeView.tsx
@@ -3,16 +3,20 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFile } from '@fortawesome/free-solid-svg-icons';
 
+import { useState } from 'react';
 import { MouseEventHandler } from 'react';
 
 interface Props {
     language: string;
     children: string;
 }
+
 const CodeView = ({ language, children }: Props) => {
+    const [ isAppear, setIsAppear ] = useState(false)
 
     const clickCopyCode: MouseEventHandler<SVGSVGElement> = () => {
         navigator.clipboard.writeText(children)
+        setIsAppear(true)
     }
 
     return (
@@ -24,15 +28,22 @@ const CodeView = ({ language, children }: Props) => {
                                    width: "100%",
                                    position: "relative",
                                    fontSize: "18px",
-                                   padding: "24px 32px",
+                                   padding: "32px",
                                    overflowX: "hidden",
+                               }}
+                               lineNumberContainerStyle ={{
+                                
                                }}
                                showLineNumbers>
                 {children}
             </SyntaxHighlighter>
-            <FontAwesomeIcon icon={faFile} 
-                             className='absolute w-6 h-6 mt-8 mr-8 text-sm text-teal-100 hover:text-teal-400 cursor-pointer'
-                             onClick={clickCopyCode}/>
+            <div className='absolute flex flex-row items-center mt-10 mr-10 p-2'>
+                {isAppear && (<span className='text-sm bold text-gray-100 mr-5 transition-transform'>Copy !</span>)}
+                <FontAwesomeIcon icon={faFile}
+                                 className='w-6 h-6 text-teal-100 hover:text-teal-400 cursor-pointer'
+                                 onClick={clickCopyCode}/>                
+            </div>
+
         </div>
     )
 };
--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Header from "./Header";
+import Header from "../organism/Header";
 import { ReactNode } from "react";
 
 interface LayoutProps {
     title: string
-    keywords: string
-    description: string
+    keywords?: string
+    description?: string
     children: ReactNode
 }
 
@@ -18,6 +18,8 @@ function Layout({ title = DEFAULT_TITLE, keywords = DEFAULT_KEYWORDS, descriptio
         <div>
             <Head>
                 <title>{title}</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1.0" />
                 <meta name="keywords" content={keywords} />
                 <meta name="description" content={description} />
                 <link rel="icon" href="/favicon.ico" />
