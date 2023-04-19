16f8931a2dfc586521e36ed9c2ef944e7086b4d6
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-04-20 00:15:42 +0900

    docs: :art: ブログ追加

---
+++ src/meta/react-next-quickstart.js
@@ -0,0 +1,14 @@
+const meta = {
+    title: 'React, Nextjsアプリの立ち上げをクイックスタートする',
+    slug: 'react-next-quickstart',
+    date: '2023/04/19',
+    excerpt: 'React, Nextjsアプリ開発時の初期状態設定を実行するためのメモ書きです。',
+    cover_image: '/images/posts/img3.jpg',
+    parent_category: 'Web',
+    category: 'React/Nextjs',
+    author: 'YORIMICHI',
+    author_image: 'https://randomuser.me/api/portraits/men/12.jpg',
+}
+
+export { meta };
+
