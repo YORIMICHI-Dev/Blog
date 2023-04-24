a92771032008a6dc28ea03b8fa3ec683b1112e65
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-04-25 01:25:27 +0900

    feat: :memo: ブログ追加

---
+++ src/meta/react-next-swap.js
@@ -0,0 +1,14 @@
+const meta = {
+    title: 'Next.jsビルド時にヒープ領域が足りなくなったのでスワップした',
+    slug: 'react-next-swap',
+    date: '2023/04/24',
+    excerpt: 'Next.jsビルド時にヒープ領域が足りなくなることがあったためスワップ領域を作成したり、ビルド時のメモリ使用量を制限したりした',
+    cover_image: '/blog/pages/404_a5lcee',
+    parent_category: 'Web',
+    category: 'React/Next.js',
+    author: 'YORIMICHI',
+    author_image: 'https://randomuser.me/api/portraits/men/12.jpg',
+}
+
+export { meta };
+
