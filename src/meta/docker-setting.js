16e08897f1dc7f6d155ee78855f920ceb00b59f3
kouhai-san <kouhaisan.yorimichi@gmail.com>
2023-05-24 01:58:08 +0900

    feat:

---
+++ src/meta/docker-setting.js
@@ -0,0 +1,14 @@
+const meta = {
+    title: 'DockerでUbuntuのコンテナを立ち上げる',
+    slug: 'docker-setting',
+    date: '2023/05/23',
+    excerpt: "UbuntuのDockerコンテナを1から立ち上げるときのメモ。",
+    cover_image: '/blog/thumbnail/docker_zpzjtr',
+    parent_category: 'Tool',
+    category: 'Docker',
+    author: 'YORIMICHI',
+    author_image: '/blog/icons/icon_r30l8j',
+}
+
+export { meta };
+
