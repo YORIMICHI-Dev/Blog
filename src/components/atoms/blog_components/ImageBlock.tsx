0092747d8ff9c1eed1a5493d61798743e8e703cc
yorimichi <yorimichi.dev@gmail.com>
2023-06-05 19:39:01 +0900

    fix: :sparkles: add width and height

--- src/components/atoms/blog_components/ImageBlock.tsx
+++ src/components/atoms/blog_components/ImageBlock.tsx
@@ -2,14 +2,21 @@ import CloudinaryImage from "@/components/molecules/CloudinaryImage";
 
 interface Props {
     imgSrc: string
+    width?: number
+    height?: number
 }
 
 
-const ImageBlock = ({imgSrc}: Props) => {
-    
+const ImageBlock = ({imgSrc, width, height}: Props) => {
+    if (width === undefined) {
+        width = 1000
+    }
+    if (height === undefined) {
+        height = 750
+    }
     return (
         <div className="flex flex-row justify-center md:my-10 my-6 rounded-lg shadow-lg">
-            <CloudinaryImage publicId={imgSrc} width={1000} height={750} alt="image" className="rounded-lg"/>
+            <CloudinaryImage publicId={imgSrc} width={width} height={height} alt="image" className="rounded-lg"/>
         </div>
     );
 }
