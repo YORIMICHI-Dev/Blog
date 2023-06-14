cf182e7fcf767556aeff35eb658f26510cbcb723
yorimichi <yorimichi.dev@gmail.com>
2023-06-15 00:37:29 +0900

    docs: :art: ブログ追加

---
+++ src/meta/vscode-conventional.js
@@ -0,0 +1,14 @@
+const meta = {
+    title: 'Conventional Commitsを使ってCommitメッセージをキレイに書く',
+    slug: 'vscode-conventional',
+    date: '2023/06/14',
+    excerpt: "VSCodeの拡張機能Conventional Commitsを使って、コミットメッセージを素早く追加機能かバグ修正かわかりやすく区別できるように書く",
+    cover_image: '/blog/thumbnail/vscode_ptniu8',
+    parent_category: 'Tool',
+    category: 'VSCode',
+    author: 'YORIMICHI',
+    author_image: '/blog/icons/icon_r30l8j',
+}
+
+export { meta };
+
