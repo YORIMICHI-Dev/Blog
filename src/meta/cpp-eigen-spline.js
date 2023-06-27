1189e8f950898f2c7fa63f9f4c0b67218f3d37e4
yorimichi <yorimichi.dev@gmail.com>
2023-06-27 22:53:55 +0900

    feat: :sparkles: ブログ作成

---
+++ src/meta/cpp-eigen-spline.js
@@ -0,0 +1,14 @@
+const meta = {
+    title: 'EigenでBスプライン曲線を描く',
+    slug: 'cpp-eigen-spline',
+    date: '2023/06/27',
+    excerpt: 'EigenのライブラリからBスプライン曲線を作成するメモ。',
+    cover_image: '/blog/thumbnail/cpp_ouw1jf',
+    parent_category: 'Code',
+    category: 'C++',
+    author: 'YORIMICHI',
+    author_image: '/blog/icons/icon_r30l8j',
+}
+
+export { meta };
+
